2d4c54b38cb7ff85479a17e8874b3eede7b8a455
adespscientist <peteradeshina3@gmail.com>
2021-07-15 17:26:21 +0100

    New commited changes
    New changes

--- Einstein/assets/js/main.js
+++ Einstein/assets/js/main.js
@@ -217,7 +217,7 @@ matchs.forEach(function(match) {
 
 // Set the date we're counting down to
 var counters=document.getElementById('date-count');
-var countDownDate = new Date("July 13   , 2021 15:37:25").getTime();
+var countDownDate = new Date("July 20   , 2021 15:37:25").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
